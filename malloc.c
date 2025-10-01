3efc69a96e58a6a0ae04718a8325727b5fbc2eec
shubham-sharma-26 <shubhamkumar933454@gmail.com.in>
2025-10-01 19:03:22 +0530

    Added malloc.c file

---
+++ malloc.c
@@ -0,0 +1,22 @@
+//malloc
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,i,*ptr;
+    printf("enter the value:");
+    scanf("%d",&n);
+
+    ptr=(int*)malloc(n*sizeof(int));
+    printf("\n enter value:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",(ptr+i));
+        printf("\n the entered values are:");
+        for(i=0;i<n;i++)
+        {
+            printf("%d",*(ptr+i));
+        }
+        free(ptr);
+    }
+}
